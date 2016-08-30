877ccd7623d965e488946fa3f64ae73b9bfd79ad
Gary Guo <nbdd0121@hotmail.com>
2016-08-30 16:37:25 +0100

    Driver update

--- bare_metal/driver/videox.c
+++ bare_metal/driver/videox.c
@@ -4,38 +4,29 @@
 #include <videox.h>
 
 #define OFFSET_LIMIT (1<<13)
-#define FUNC_MAX 9
-
-void videox_init() {
-}
+#define FUNC_MAX 2
 
 static void videox_write_src(uint64_t val) {
+    volatile uint32_t *reg = (volatile uint32_t *)DEV_MAP__io_ext_video_acc_inst__BASE;
+    while(reg[0] == 128);
+    reg[0] = (uint32_t)val;
+    reg[1] = (uint32_t)(val >> 32);
+}
+
+static void videox_write_dest(uint64_t val) {
     volatile uint32_t *reg = (volatile uint32_t *)DEV_MAP__io_ext_video_acc_inst__BASE;
     while(reg[2] == 128);
     reg[2] = (uint32_t)val;
     reg[3] = (uint32_t)(val >> 32);
 }
 
-static void videox_write_dest(uint64_t val) {
-    volatile uint32_t *reg = (volatile uint32_t *)DEV_MAP__io_ext_video_acc_inst__BASE;
-    while(reg[4] == 128);
-    reg[4] = (uint32_t)val;
-    reg[5] = (uint32_t)(val >> 32);
-}
-
-static void videox_write_inst(uint32_t val) {
-    volatile uint32_t *reg = (volatile uint32_t *)DEV_MAP__io_ext_video_acc_inst__BASE;
-    while(reg[0] == 128);
-    reg[0] = val;
-}
-
 void videox_wait() {
     volatile uint32_t *reg = (volatile uint32_t *)DEV_MAP__io_ext_video_acc_inst__BASE;
-    while(reg[0] != 0);
+    while(reg[2] != 0);
 }
 
-void videox_exec(int func, const void* src, void* dest, size_t len) {
-    if (func < FUNC_MOV || func > FUNC_MAX) {
+void videox_exec(int func, const void* src, void* dest, size_t len, int attrib) {
+    if (func > FUNC_MAX) {
         printf("Invalid function number %d", func);
         return;
     }
@@ -46,9 +37,8 @@ void videox_exec(int func, const void* src, void* dest, size_t len) {
         return;
     }
 
-    videox_write_src((uintptr_t)src << 21 | len << 5 | 1);
-    videox_write_dest((uintptr_t)dest << 21 | len << 5 | 1);
-    videox_write_inst(func);
+    videox_write_src((uintptr_t)src | ((uintptr_t)len << 34) | (1ULL << 55) | func | ((attrib & 63ull) << 56));
+    videox_write_dest((uintptr_t)dest | ((uintptr_t)len << 34));
 }
 
 void* fast_memcpy(void* dest, const void* src, size_t cnt) {
@@ -73,13 +63,11 @@ void* fast_memcpy(void* dest, const void* src, size_t cnt) {
         cnt -= 64 - diffcnt;
     }
 
-    videox_write_inst(8);
-
     while (cnt >= 64) {
         size_t transcnt = cnt > 0x40000 ? 0x40000 : cnt &~ 63;
         int last = cnt <= 0x40000;
-        videox_write_src((uintptr_t)src << 21 | transcnt << 5 | last);
-        videox_write_dest((uintptr_t)dest << 21 | transcnt << 5 | last);
+        videox_write_src((uintptr_t)src | ((uintptr_t)transcnt << 34) | ((uintptr_t)last << 55));
+        videox_write_dest((uintptr_t)dest | ((uintptr_t)transcnt << 34));
         src += transcnt;
         dest += transcnt;
         cnt -= transcnt;
--- bare_metal/driver/videox.h
+++ bare_metal/driver/videox.h
@@ -4,13 +4,10 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#define FUNC_MOV 8 
+#define FUNC_MOV 0 
 
-void videox_init();
-void videox_add_inst(uint32_t val);
-uint32_t videox_peak_inst();
 void videox_wait();
-void videox_exec(int func, const void* src, void* dest, size_t len);
+void videox_exec(int func, const void* src, void* dest, size_t len, int attrib);
 void* fast_memcpy(void* dest, const void* src, size_t cnt);
 
 #endif
